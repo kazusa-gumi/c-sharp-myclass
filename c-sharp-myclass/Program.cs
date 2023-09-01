d99708ffc670fc456fe3c2ab3d331ce58d7c7b0b
kazusa-gumi <jsva4127@yahoo.co.jp>
2023-09-01 11:22:01 +1000

    aaa

--- c-sharp-myclass/Program.cs
+++ c-sharp-myclass/Program.cs
@@ -9,61 +9,17 @@ namespace HelloWorld
         // メソッド
         static void Main(string[] args)
         {
-            // while文
-            int i = 0;
-            int j = 3;
-            while (i < j)
-            {
-                int MyInt1 = Convert.ToInt32(Console.ReadLine());
-                int MyInt2 = Convert.ToInt32(Console.ReadLine());
+            string[] MyArray = { "Mon", "Tue", "Web" };
+            //for(int i =0; i < MyArray.Length; i++)
+            //{
+            //    Console.WriteLine(MyArray[i]);
+            //}
 
-                int Result = MyInt1 + MyInt2;
-                switch (Result)
-                {
-                    case 10:
-                        Console.WriteLine("The number is 10");
-                        break;
-                    case 12:
-                        Console.WriteLine("The number is 12");
-                        break;
-
-                    default:
-                        Console.WriteLine("The number is not found, Try Again!");
-                     break;
-                }
-
-                Console.WriteLine("The Sum = " + Result);
-                i++;
+            // foreach文
+            // 配列の要素１つ１つを取り出して表示しているイメージかな。
+            foreach(var item in MyArray) {
+                Console.WriteLine(item);
             }
-            // for文
-         
-                //int MyInt1 = Convert.ToInt32(Console.ReadLine());
-                //int MyInt2 = Convert.ToInt32(Console.ReadLine());
-
-                //int Result = MyInt1 + MyInt2;
-
-                //if (Result == 10) // if yes will print found,
-                // if no will jump to not found message
-
-
-                //{
-                //    Console.WriteLine("We found the sum = " + Result);
-                //    Console.WriteLine("Break");
-                //    // stop the loop
-                //    break;
-                //}
-                //else
-                //{
-                //    Console.WriteLine("The number is not found, Try Again!");
-                //}
-
-                // like a door
-                
-
-                
-            
-
-            
         }
     }
 }
